80accf08cba1a02a1ef17b8878f566f506c4b05e
omerokumus <omerokumus3@gmail.com>
2022-03-07 19:05:22 +0300

    promise cont'd

--- Asynchronous/ch2_promises.js
+++ Asynchronous/ch2_promises.js
@@ -17,3 +17,44 @@
     newPromise(f1)
     newPromise(f2)
 }
+
+
+{
+    let promise = new Promise(function (resolve, reject) {
+        setTimeout(() => resolve("success value"), 5000)
+    });
+
+
+
+    promise.then(
+        () => console.log("success"),
+        () => console.log("error")
+    )
+
+
+     promise = new Promise(function (resolve, reject) {
+        setTimeout(() => reject("reject value"), 3000)
+    });
+
+    promise.then(
+        // resolve ve reject fonk. içine verilen argumanlar error ve result olarak buraya gönderilir
+        (result) => console.log(result),
+        (error) => console.log(error)
+    )
+
+    promise = new Promise(function (resolve, reject) {
+        setTimeout(() => resolve(201), 1000)
+    });
+
+    promise.then(result => printMessage(result))
+
+    function printMessage(result){
+        if (result === 201)
+            console.log("201 success")
+        else if (result === 200)
+            console.log("200 success")
+        else
+            console.log("printMessage: success")
+    }
+
+}
