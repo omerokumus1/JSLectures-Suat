03a33c48e14e1d66407a6ecbd847f38d69942553
omerokumus <omerokumus3@gmail.com>
2022-06-07 17:30:33 +0300

    objects basics finished

--- P4_Objects/p4_1_basics.js
+++ P4_Objects/p4_1_basics.js
@@ -9,7 +9,8 @@ let o3 = {
     school: "Marmara University",
     "profession": "Software Developer",
     "likes sport": true,
-    likesSport: true
+    likesSport: true,
+    lectures: [1, 2, 3, 4]
 }
 
 
@@ -28,8 +29,12 @@ if (prop === "name")
 else if (prop === "age")
     console.log(o3.age);
 
+for (const lecture of o3.lectures) { // foreach in java
+
+}
+
 /*
-    square brackets access vs. dot access: Eğer key değeri kullanıcıya bağlıysa veya bir fonksiyon tarafından hesaplanacaksa
+    square brackets access vs. dot access: Eğer key değeri kullanıcıya bağlıysa veya bir fonksiyon tarafından hesaplanacaksa (olasılık varsa)
         square brackets, eğer böyle bir durum yoksa yani statikse dot access kullanabilirsin.
  */
 
@@ -46,7 +51,7 @@ console.log(o3)
 
 function getRandomProperty() {
     const arr = ["name", "age", "school", "profession"];
-    return arr[Math.floor(Math.random()*4)];
+    return arr[Math.floor(Math.random() * 4)];
 }
 
 
@@ -73,6 +78,7 @@ console.log(plot[yAxis])
 // yukarıda yaptığımız objeler bir kez daha üretilmek istenirse copy-paste yapmak zorundayız. Fakat bu yöntemle
 // copy paste yapmadan aynı yapıda obje üretebiliriz
 // aşağıdaki fonksiyon değişken değerleri parametre olarak alır ve gerekli objeyi üretir
+// üretilen objenin tipi Object olur.
 function person(name, age, school, profession) {
     return {
         "name": name,
@@ -81,7 +87,6 @@ function person(name, age, school, profession) {
         "profession": profession
     }
 }
-
 
 
 const omer = person("Ömer Okumuş", "25", "Marmara University", "Software Engineer")
@@ -104,10 +109,28 @@ const omer2 = person2("Ömer Okumuş", "25", "Marmara University", "Software Eng
 const suat2 = person2("Suat Öztürk", "25", "Kennesaw", "Software Development Engineer in Test")
 
 console.log(omer2)
+console.log(typeof omer2)
 console.log(suat2)
 
 // property availability check
 console.log("omer contains 'name': " + ("name" in omer))
 console.log("omer contains 'surname': " + ("surname" in omer))
-// gelen requestte belirli alanların olmasını istiyorsan bu şekilde kontrol yaparsın. Eğer yoksa bad request döndürürsün varsa işlemi yaparsın
+// gelen requestte belirli alanların olmasını istiyorsan bu şekilde kontrol yaparsın.
+// Eğer yoksa bad request döndürürsün varsa işlemi yaparsın
+
+
+// for..in loop: her döngü iterasyonunda anahtarı verir. Bu anahtar sayesinde objelere square brackets access ile erişim yapılabilir
+for (const key in omer2) {
+    console.log(omer2[key])
+}
+
+// for-in vs for-of: for-in döngüsü anahtarları verirken for-of döngüsü direkt değerleri verir.
+for (const val of [10, 20, 30, 40, 50]) {
+    console.log(val)
+}
+for (const key in [10, 20, 30, 40, 50]) {
+    console.log(key)
+}
+
+
 
