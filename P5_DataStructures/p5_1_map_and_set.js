3dfeae62e75e84a05bfeee0e69b4ef0f508cc312
omerokumus <omerokumus3@gmail.com>
2022-06-29 17:23:56 +0300

    maps and sets are finished

--- P5_DataStructures/p5_1_map_and_set.js
+++ P5_DataStructures/p5_1_map_and_set.js
@@ -66,12 +66,12 @@
     }
 
     // satır satır
-    for (const row of m1) {
+    for (const row of m1) { // m1 yerine m1.entries() kullanılabilir
         console.log(row);
     }
 
     // foreach
-    m1.forEach( (val, key, map) => {
+    m1.forEach((val, key, map) => {
         console.log(key + ": " + val)
     })
 
@@ -79,7 +79,128 @@
     //     console.log(map)
     // })
 
+}
 
+{ // C. Map from objects
 
+    // 1- Map from Array
+    // arrayler 2 boyutlu olmalı ve her bir arrayde 2 tane değer olmalı. 2 den fazlaysa ilk 2'sini alır,
+    // eksikse olmayanlar undefined olur
+    // [ [a, b], [c, d], .. ]
+
+    let m1 = new Map([
+        [1, 2],
+        [3, 4],
+        [5, 6]
+    ]);
+    console.log(m1);
+
+    let a1 = [
+        [1, 2],
+        [], // eleman eksikse undefined
+        [5, 6, 3] // 3 dahil edilmez
+    ];
+    let m2 = new Map(a1);
+    console.log(m2);
+
+    // 2- Map from Object
+    function person(name, age) {
+        return {
+            name, age
+        };
+    }
+
+    let p1 = person("ömer", 25);
+    let p2 = person("faruk", 24);
+
+    // Object.entries: herbir property ve value için array döndürür. Bütün property-value arrayler başka bir array içinde bulunur
+    // Örn; [ [name, 'ömer'], [age, 25] ]
+    console.log(Object.entries(p1));
+
+    m1 = new Map(Object.entries(p1));
+    console.log(m1);
+
+}
+
+{ // D. Object from Map
+    // Object.fromEntries: 2 boyutlu 2 şer elemana sahip diziler bulunduran diziyi input olarak alır.
+    let a1 = [
+        [1, 2],
+        [3, 4],
+        [5, 6]
+    ];
+    let o1 = Object.fromEntries(a1);
+    console.log(o1);
+
+    let m1 = new Map();
+    m1.set("istanbul", "200")
+        .set("ankara", "100")
+        .set("trabzon", "500")
+        .set("igdir", "200")
+        .set("rize", "700");
+
+    // m1.entries: satırları eleman olarak tutan 2 boyutlu dizi döndürür
+    console.log(m1.entries());
+
+    o1 = Object.fromEntries(m1.entries()); // m1 yazsak da olur
+    console.log(o1);
+}
+
+{ // A. Set Basics
+    // sadece value vardır, key yoktur
+    // her bir value 1 kere eklenebilir
+
+    // 1- set construction
+    let s1 = new Set();
+    s1.add("istanbul")
+        .add("trabzon")
+        .add("trabzon") // duplication otomatik olarak göz ardı edilir
+        .add("rize");
+    console.log(s1);
+
+    // 2- has()
+    console.log(s1.has("trabzon"));
+    console.log(s1.has("kayseri"));
+
+    // 3- set.clear()
+    // 4- set.delete
+    s1.delete("rize");
+    console.log(s1);
+
+    // 5- set.size
+
+    // Ex; duplicated kullanıcı engellemek
+    let s2 = new Set()
+    s2.add("Ömer")
+        .add("Faruk")
+        .add("Suat")
+        .add("Ahmet");
+
+    let setSize = s2.size;
+    s2.add("Ömers");
+    if (setSize === s2.size)
+        console.log("Hakkınızı zaten kullandınız.")
+    else
+        console.log("Tebrikler! AirPods kazandınız.")
+
+    // 6- Set from Array
+    let a1 = [1, 2, 3, 4, 4, 5, 6, 6, 7];
+    s2 = new Set(a1);
+    console.log(s2);
+
+    // 7- Iteration over Set
+    s2 = new Set([1, 2, 3, 4, 5, 6, 7])
+    for (const val of s2) {
+        console.log(val);
+    }
+
+    s2.forEach((val, val2, set) => {
+        console.log(val2);
+    })
+
+    // keys, values, entries
+    console.log(s2.keys());
+    console.log(s2.values());
+    console.log(s2.entries());
 
 }
