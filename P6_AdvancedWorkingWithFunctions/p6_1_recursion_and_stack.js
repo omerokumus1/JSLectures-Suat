8f28772f52da6b7569c141548439410037a323ac
omerokumus <omerokumus3@gmail.com>
2022-08-23 17:37:50 +0300

    Recursive Traversals intro

--- P6_AdvancedWorkingWithFunctions/p6_1_recursion_and_stack.js
+++ P6_AdvancedWorkingWithFunctions/p6_1_recursion_and_stack.js
@@ -33,11 +33,11 @@
     // Function: f(n) = f(n-2) + n
     // Base case: f(2) = 2 (veya f(0) = 0)
     function f2(n) {
-        if (n < 0)
+        if (n < 0) // input check
             return -Infinity;
-        if (n === 2)
+        if (n === 2) // base case
             return 2;
-        return f2(n-2) + n;
+        return f2(n-2) + n; // recursive step
     }
     n = 6;
     console.log(f2(n));
@@ -46,10 +46,44 @@
     // Örn; n!
     // Fibonacci: f(n) = f(n-1) + f(n-2)
 
-    // Recursive fonksiyonların memory yükü
+    // Recursive fonksiyonların memory yükü: yukarıda f() -> recursive vs f1() -> iteration
     // 2^4 = 2^3 * 2
     // 2^3 = 2^2 * 2
     // 2^2 = 2^1 * 2
     // 2^1 = 2^0 * 2
     // 2^0 = 1
+}
+
+{ // Recursive Traversals
+    // Örn; aşağıdaki objede toplam salary'i recursive olarak bul
+    let company = { // the same object, compressed for brevity
+        sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 1600 }],
+        development: {
+            sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
+            internals: [{name: 'Jack', salary: 1300}]
+        }
+    };
+
+    // 1. Array görürsen içerisinde gezin ve salary değerlerini topla
+    // 2. Obje görürsen içerisinde gezin ve array bulmaya çalış
+    // Base case -> array bulmak
+    // Recursive step -> array aramak
+    function sumSalariesRecursively(obj) {
+        if (Array.isArray(obj)) { // base case: array bulundu, salary'leri topla
+            let total = 0;
+            for (const value of obj) {
+                total += value.salary;
+            }
+            return total;
+        } else { // recursive step: array bulunamadı, aramaya devam et
+            let totalSalary = 0;
+            for (const value of Object.values(obj)) {
+                totalSalary += sumSalariesRecursively(value);
+            }
+            return totalSalary;
+        }
+    }
+
+    console.log(sumSalariesRecursively(company));
+
 }
