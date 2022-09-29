a0d1c9bc85881ab6295766bd2403742e766f723d
omerokumus <omerokumus3@gmail.com>
2022-09-29 11:14:04 +0300

    nfe finished

--- P6_AdvancedWorkingWithFunctions/p6_6_function_object_nfe.js
+++ P6_AdvancedWorkingWithFunctions/p6_6_function_object_nfe.js
@@ -50,3 +50,67 @@ wrapper.calledParams = [];
 wrapper(11,22);
 wrapper(11,22);
 console.log(wrapper.calledParams)
+
+/**
+ *  - NFE: Named Function Expression
+ *      1. Function Expression'lara isim vererek oluşturulur
+ *          let myFunc = function func(){...}
+ *      2. Bu yapı sayesinde güvenli bir şekilde recursive call yapabiliriz.
+ *      3. func ismi ile dışarıdan çağrı yapılamaz çünkü func function-local olarak tanımlanır
+ *      4. const ile tanımlarsak yine güvenli bir recursive çağrı yapabiliriz
+ *
+ */
+
+
+// 1. için örnek
+let myFunc = function func(name) {
+    if (name)
+        console.log(`Hello ${name}`);
+    else
+        console.log("Hello Outsider");
+}
+
+// 2. için örnek
+let myFunc2 = function func(name) {
+    if (name)
+        console.log(`Hello ${name}`);
+    else
+        func("Outsider");
+}
+
+// Neden güvenli?
+let myFunc3 = function(name) {
+    if (name)
+        console.log(`Hello ${name}`);
+    else
+        myFunc3("Outsider"); // myFunc3 başka bir şeye assign edilirse burası hatalı çalışır
+}
+
+let newFunc = myFunc3;
+myFunc3 = null;
+// newFunc(); // yukarıdaki fonksiyon şu hatayı verir: TypeError: myFunc3 is not a function
+
+myFunc3 = function (){ // Yukarıdaki myFunc3 tanımında logic error'a sebebiyet verir
+    console.log("New function for myFunc3");
+}
+myFunc3();
+
+// Bu tip durumlar named function'lar ile gerçekleşmez. Çünkü NFE, function expression'un kendisini
+// çağırır.
+
+
+// 3. için örnek
+// func("Ömer"); // ReferenceError: func is not defined
+
+
+// 4. için örnek
+const myFunc4 = function (name){
+    if (name)
+        console.log(`Hello ${name}`);
+    else
+        myFunc4("Outsider");
+}
+// Yeniden assign edilemeyeceği için myFunc4 başka bir şeye referans veremez ve recursive çağrı
+// daima myFunc4'ü çağırır
+// myFunc4 = null;
+
